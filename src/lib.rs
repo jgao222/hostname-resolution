1eeddeeec4197d39bd2ba5500a40df3927abffb7
jgao2 <jgao222@uw.edu>
2022-08-31 19:49:38 -0700

    server main functionality works

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
 use std::{collections::HashMap};
 use anyhow::{Result, bail, anyhow};
 
+#[derive(Debug)]
 pub enum HttpRequestMethod {
     GET,
     POST,
     DELETE,
 }
 
+#[derive(Debug)]
 pub struct HttpRequest {
-    method: HttpRequestMethod,
-    uri: String,
-    version: String,
-    headers: HashMap<String, String>,
-    content: Vec<u8>
+    pub method: HttpRequestMethod,
+    pub uri: String,
+    pub version: String,
+    pub headers: HashMap<String, String>,
+    pub content: Vec<u8>
 }
 
+#[derive(Default)]
 pub struct HttpResponse {
-    version: String,
-    status: u32,
-    reason: String,
-    headers: Vec<(String, String)>,
-    content: Vec<u8>
+    pub version: String,
+    pub status: u32,
+    pub reason: String,
+    pub headers: Vec<(String, String)>,
+    pub content: Vec<u8>
 }
 
 pub struct HostnameHandler {
@@ -35,8 +38,21 @@ impl TryFrom<String> for HttpRequest {
     }
 }
 
+impl HttpResponse {
+    pub fn bad_request() -> Self {
+        HttpResponse { version: "HTTP/1.1".into(), status: 400, reason: "Bad Request".into(), ..Default::default() }
+    }
+
+    pub fn not_found() -> Self {
+        HttpResponse { version: "HTTP/1.1".into(), status: 404, reason: "Not Found".into(), ..Default::default() }
+    }
+}
+
 fn parse_http_request(request: &String) -> Result<HttpRequest> {
-    let lines: Vec<&str> = request.lines().collect();
+    let lines: Vec<&str> = request.split("\r\n").collect(); // only split crlf, not \n
+    if lines.len() < 1 {
+        bail!("Unable to parse HTTP Requst: Empty")
+    }
     // strip crlf off end of line so it doesn't get parsed as part of http ver
     let start_line: Vec<&str> = lines[0].split_whitespace().collect();
     if start_line.len() != 3 {
@@ -53,11 +69,12 @@ fn parse_http_request(request: &String) -> Result<HttpRequest> {
     let mut headers = HashMap::new();
 
     let mut idx = 1;
-    while lines[idx] != "\r\n" {
+    while idx < lines.len() && !lines[idx].starts_with("\n") {
         let mut key_value = lines[idx].split(":");
+        // println!("{key_value:?}");
         let key = key_value.next().ok_or(anyhow!("Bad header line"))?;
-        let value = key_value.next().ok_or(anyhow!("Bad header line"))?;
-        headers.insert(key.to_string(), value.to_string());
+        let value = key_value.next().ok_or(anyhow!("Bad header line"))?.trim();  // leading and trailing whitespace is optional for header fields
+        headers.insert(key.to_string().to_lowercase(), value.to_string());
 
         idx += 1;
     }
@@ -65,11 +82,58 @@ fn parse_http_request(request: &String) -> Result<HttpRequest> {
     // now process the body
     let mut content = Vec::new();
     // hopefully this takes just the remaining lines
-    for line in lines {
+
+    for line in lines[idx..].iter() {
         content.extend(line.bytes());
     }
 
     Ok(HttpRequest { method: req_type, uri: start_line[1].to_string(), version: start_line[2].to_string(), headers, content })
+}
+
+
+/// Parse query params from a uri
+fn parse_query_params(uri: &String) -> HashMap<String, String>{
+    let params_block = uri.rsplit_once("?");
+    let params_block = match params_block {
+        Some((_, s2)) => s2,
+        None => return HashMap::new()
+    };
+    parse_key_value_list(params_block)
+}
+
+/// Parse form parameters from a POST request
+/// # Preconditions
+/// - `post_request` is an `HttpRequest` with method `HttpRequestMethod::POST`
+/// - the post request has been made with the header
+fn parse_form_params(post_request: &HttpRequest) -> HashMap<String, String> {
+    let out = HashMap::new();
+    if let HttpRequestMethod::POST = post_request.method {
+        if let Some(t) = post_request.headers.get("content-type") {
+            if t != "application/x-www-form-urlencoded" {
+                return out;
+            }
+        } else {
+            return out;
+        }
+        // the body should be in key=value list format now
+        parse_key_value_list(&String::from_utf8_lossy(&post_request.content))
+    } else {
+        out
+    }
+}
+
+fn parse_key_value_list(list: &str) -> HashMap<String, String> {
+    let mut out = HashMap::new();
+    let param_pairs = list.split("&");
+    // now pairs of the form `key=value`
+    for pair in param_pairs {
+        let equals_index = match pair.find("=") {
+            Some(i) => i,
+            None => continue
+        };
+        out.insert(pair[..equals_index].to_string(), pair[equals_index + 1..].to_string());
+    }
+    out
 }
 
 
@@ -79,32 +143,67 @@ impl HostnameHandler {
     }
 
     pub fn handle_request(&mut self, parsed_request: &HttpRequest) -> HttpResponse {
+        println!("{parsed_request:?}");
+        println!("{}", String::from_utf8_lossy(&parsed_request.content));
         // need to parse out the hostname in the request
         // uh, query parameters?
-        match parsed_request.method {
-            HttpRequestMethod::GET => {
+        let query_params = parse_query_params(&parsed_request.uri);
+        let form_params = if let HttpRequestMethod::POST = parsed_request.method {
+            parse_form_params(parsed_request)
+        } else {
+            HashMap::new()
+        };
+        println!("post params: {form_params:?}");
 
+        let result = match parsed_request.method {
+            HttpRequestMethod::GET => {
+                let hostname = match query_params.get("hostname") {
+                    Some(name) => name,
+                    None => return HttpResponse::bad_request()
+                };
+                self.handle_get(hostname)
             }
             HttpRequestMethod::POST => {
-
+                let hostname = match form_params.get("hostname") {
+                    Some(name) => name,
+                    None => return HttpResponse::bad_request()
+                };
+                let host_value = match form_params.get("host_value") {
+                    Some(value) => value,
+                    None => return HttpResponse::bad_request()
+                };
+                self.handle_post(hostname.clone(), host_value.clone())
             }
             HttpRequestMethod::DELETE => {
-                HostnameHandler::handle_delete(parsed)
+                let hostname = match query_params.get("hostname") {
+                    Some(name) => name,
+                    None => return HttpResponse::bad_request()
+                };
+                self.handle_delete(hostname)
             }
+        };
+
+        result
+    }
+
+    fn handle_get(&self, hostname: &String) -> HttpResponse {
+        let value = self.hosts_to_hashes.get(hostname);
+        match value {
+            Some(value) => {
+                let headers: Vec<(String, String)> = vec![("content-type".into(), "text/plain".into()), ("content-length".into(), value.len().to_string())];
+                HttpResponse { version: "HTTP/1.1".into(), status: 200, reason: "OK".into(), headers, content: value.as_bytes().to_vec() }
+            }
+            None => HttpResponse::not_found()
         }
     }
 
-    fn handle_get(hostname: String) -> Result<()> {
-
-        todo!()
+    fn handle_post(&mut self, hostname: String, host_value: String) -> HttpResponse {
+        self.hosts_to_hashes.insert(hostname, host_value);
+        println!("==== internal state is now: {:?}", self.hosts_to_hashes);
+        HttpResponse { version: "HTTP/1.1".into(), status: 200, reason: "OK".into(), headers: vec![], content: vec![] }
     }
 
-    fn handle_post(hostname: String, host_value: String) -> Result<()> {
-
-        todo!()
-    }
-
-    fn handle_delete(hostname: String) {
+    fn handle_delete(&mut self, hostname: &String) -> HttpResponse {
         // there seem to be security issues here
         // need to store more data than the current model allows to verify
         // that it is indeed the original host requesting to delete their entry
@@ -119,11 +218,13 @@ impl Into<Vec<u8>> for HttpResponse {
 
         out.push_str(&format!("{} {} {}\r\n", self.version, self.status, self.reason));
         for (key, val) in self.headers {
-            out.push_str(&format!("{}: {}", key, val));
+            out.push_str(&format!("{}: {}\r\n", key, val));
         }
 
+        out.push_str("\r\n"); // extra line before body
         let mut out: Vec<u8> = out.into_bytes();
         out.extend(self.content);
+        out.extend("\r\n".as_bytes());
 
         out
     }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::env;
 use std::error::Error;
-use std::io::{Read, Write};
+use std::io::{Read, Write, ErrorKind};
 use std::net::{TcpListener};
+use std::process::exit;
 
 use hostname_resolution_server::*;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args().collect::<Vec<String>>();
-    println!("args are {:?}", args);
+    // println!("args are {:?}", args);
 
+    if args.len() < 2 {
+        eprintln!("Usage: ./hostname_resolution_server [PORT]");
+        exit(1)
+    }
     let port = args[1].clone();
 
     let addr = format!("127.0.0.1:{port}");
@@ -20,9 +25,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut host_handler = HostnameHandler::new();
 
     // loop accepting connections forever
+    let mut buf: [u8; 256] = [0; 256];
     loop {
         let mut socket = match listener.accept() {
-            Ok((socket, _)) => {
+            Ok((socket, addr)) => {
+                println!("{addr:?}");
                 socket
             }
             Err(e) => {
@@ -31,21 +38,61 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         };
         let mut request = String::new();
-        match socket.read_to_string(&mut request) {
-            Ok(bytes) => {
-                assert!(request.len() == bytes);
+        let mut read_success = true;
+        while !request.contains("\r\n\r\n") {
+            println!("{}", escape_escapes(&request));
+            let bytes_read = match socket.read(&mut buf) {
+                Ok(bytes_read) => bytes_read,
+                Err(e) => match e.kind() {
+                    ErrorKind::Interrupted => continue,
+                    _ => { read_success = false; break }
+                },
+            };
+            if bytes_read == 0 {
+                read_success = false;
+                break;
             }
-            Err(e) => {
-                eprintln!("{e:?}");
-                continue;
-            }
+            request.push_str(&String::from_utf8_lossy(&buf[0..bytes_read]));
+            println!("Two CRLF exist: {:?}", request.find("\r\n\r\n"))
         }
+        println!("{}", escape_escapes(&request));
+        if !read_success {
+            continue;
+        }
+        let (request_heading, mut request_rest) = request.rsplit_once("\r\n\r\n").unwrap();
 
         // parse and handle request
-        let req_parsed = match HttpRequest::try_from(request) {
+        let mut req_parsed = match HttpRequest::try_from(request_heading.to_string()) {
             Err(e) => { eprintln!("{e:?}"); continue },
             Ok(s) => s
         };
+        // yep, this feels bad, but it must be done
+        // looking for two CRLF in a row only works for requests without bodies (thanks 333 :|)
+        if let Some(byte_count) = req_parsed.headers.get("content-length") {
+            let byte_count = byte_count.parse().unwrap();
+            // keep reading into request_rest until it has at least byte_count bytes
+            let mut request_rest = request_rest.to_string();
+            while request_rest.len() < byte_count {
+                // TODO this code is duplicated from above
+                let bytes_read = match socket.read(&mut buf) {
+                    Ok(bytes_read) => bytes_read,
+                    Err(e) => match e.kind() {
+                        ErrorKind::Interrupted => continue,
+                        _ => { read_success = false; break }
+                    },
+                };
+                if bytes_read == 0 {
+                    read_success = false;
+                    break;
+                }
+                request_rest.push_str(&String::from_utf8_lossy(&buf[0..bytes_read]));
+            }
+            if !read_success {
+                continue;
+            }
+            let content_bytes = request_rest.as_bytes();
+            req_parsed.content.extend_from_slice(&content_bytes[0..byte_count]);
+        }
         let res = host_handler.handle_request(&req_parsed);
         let res_bytes: Vec<u8> = res.into();
 
@@ -56,4 +103,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn escape_escapes(source: &String) -> String {
+    source.replace("\n", "\\n").replace("\r", "\\r")
 }
